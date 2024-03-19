3965dcb1d32ccb42eef91bf7764bcfea2bdf1a6c
Uxgna <Uxgna@10.242.168.187>
2024-03-19 09:56:42 -0400

    Part 1 + Part 2

--- src/virtualpet/VirtualPet.java
+++ src/virtualpet/VirtualPet.java
@@ -14,7 +14,36 @@ public class VirtualPet {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner s = new Scanner(System.in);
+        //Part 1: Menu
+        String pet = "";
+        System.out.println("Welcome to your virutal pet Simulator");
+        System.out.println("^-^");
+        while(true){
+            //Splash Screen
+            System.out.println("1. Start\n2. Instructions\n3. Exit");
+            String option = s.next();
+            //Part 2: Decision Structures
+            //Switch statement for option
+            switch (option) {
+                case "1":
+                case "Start":
+                    System.out.println("What pet do you want");
+                    
+                    break;
+                case "2":
+                case "Instructions":
+                    System.out.println("Instructions");
+                    break;
+                case "3":
+                case "Exit":
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Please enter a valid input");
+                    break;
+            }
+        }
     }
     
 }
